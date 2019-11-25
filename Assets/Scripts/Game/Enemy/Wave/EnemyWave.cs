2c538ab27b18840a25849687966d3b57d5454b64
Asher Hennig [Student] <asher.hennig@student.sl.on.ca>
2019-11-25 16:09:45 -0500

    added enemyWave Script

---
+++ Assets/Scripts/Game/Enemy/Wave/EnemyWave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//1
+[Serializable]
+public class EnemyWave : MonoBehaviour
+{
+    //2
+    public int pathIndex;
+    //3
+    public float startSpawnTimeInSeconds;
+    //4
+    public float timeBetweenSpawnInSeconds = 1f;
+    //5
+    public List<GameObject> listOfEnemies = new List<GameObject>();
+}
