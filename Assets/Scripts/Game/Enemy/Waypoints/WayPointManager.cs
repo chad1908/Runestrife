8493e51c91c094ca0940bcd17f270120bf5a336f
chad <hockey_1908@hotmail.com>
2019-11-25 15:16:10 -0500

    Starter for TD chapter 18 (Finished up to Enemies)

---
+++ Assets/Scripts/Game/Enemy/Waypoints/WayPointManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+public class WayPointManager : MonoBehaviour
+{
+    //A static variable that holds a reference to this script.
+    public static WayPointManager Instance;
+    //The list of stored paths.
+    public List<Path> Paths = new List<Path>();
+    void Awake()
+    {
+        //Sets the value explained in entry 1.
+        Instance = this;
+    }
+    //Returns the position of the enemy spawn point based on what path they will take.
+    public Vector3 GetSpawnPosition(int pathIndex)
+    {
+        return Paths[pathIndex].WayPoints[0].position;
+    }
+}
+//Path is defined here as a class containing a list of waypoints, which are stored as Transforms.
+[System.Serializable]
+public class Path
+{
+    public List<Transform> WayPoints = new List<Transform>();
+}
