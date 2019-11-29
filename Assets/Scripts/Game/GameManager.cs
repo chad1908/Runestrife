9a563b2c95eb261cbc727e66d0257ca1bcca4723
Alexander Collings [Student] <alexander.collings@student.sl.on.ca>
2019-11-29 13:21:27 -0500

    Created GameManager, requires WaveManager and EnemyManager to function

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public int gold;
+    public int waveNumber;
+    public int escapedEnemies;
+    public int maxAllowedEscapedEnemies = 5;
+    public bool enemySpawningOver;
+    public AudioClip gameWinSound;
+    public AudioClip gameLoseSound;
+
+    private bool gameOver;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (!gameOver && enemySpawningOver)
+        {
+            // Check if there are no enemies left
+            if (waveNumber == 50) // if (EnemyManager.Instance.Enemies.Count == 0)
+            {
+                OnGameWin();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitToTitleScreen();
+        }
+    }
+
+    private void OnGameWin()
+    {
+        AudioSource.PlayClipAtPoint(gameWinSound, Camera.main.transform.position);
+        gameOver = true;
+    }
+
+    public void QuitToTitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void OnEnemyEscape()
+    {
+        escapedEnemies++;
+        if (escapedEnemies == maxAllowedEscapedEnemies)
+        {
+            OnGameLose();
+        }
+    }
+
+    private void OnGameLose()
+    {
+        gameOver = true;
+
+        AudioSource.PlayClipAtPoint(gameLoseSound, Camera.main.transform.position);
+        // EnemyManager.Instance.DestroyAllEnemies();
+        // WaveManager.Instance.StopSpawning();
+    }
+
+    public void RetryLevel()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
