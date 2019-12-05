d06a1323cd74d4d0ee71b2053cb3d98030cf3d42
Humza Khan <humza.khan@msn.com>
2019-12-05 11:50:17 -0500

    Added TowerManager script

---
+++ Assets/Scripts/Game/Tower/TowerManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+[Serializable]
+public struct TowerCost
+{
+    public TowerType TowerType; //--------------------------------Waiting on Asher's part to finish
+    public int Cost;
+}
+
+public class TowerManager : MonoBehaviour
+{
+    //As this is another singleton, a static reference to this script is stored away in Instance
+    public static TowerManager Instance;
+
+    //References to the tower prefabs
+    public GameObject stoneTowerPrefab;
+    public GameObject fireTowerPrefab;
+    public GameObject iceTowerPrefab;
+
+    //Store the cost of each type of tower in a list
+    public List<TowerCost> TowerCosts = new List<TowerCost>();
+
+    //Sets the Instance variable to this script
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    //Accepts a tower slot and a type of tower as parameters, and creates a new copy of the chosen tower at the position of the tower slot
+    public void CreateNewTower(GameObject slotToFill, TowerType towerType) //--------------------------------Waiting on Asher's part to finish
+    {
+        switch (towerType)
+        {
+            case towerType.Stone:
+                Instantiate(stoneTowerPrefab, slotToFill.transform.position, Quaternion.identity);
+                slotToFill.gameObject.SetActive(false);
+                break;
+            case towerType.Fire:
+                Instantiate(fireTowerPrefab, slotToFill.transform.position, Quaternion.identity);
+                slotToFill.gameObject.SetActive(false);
+                break;
+            case towerType.Ice:
+                Instantiate(iceTowerPrefab, slotToFill.transform.position, Quaternion.identity);
+                slotToFill.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    //a LINQ utility method to easily get the price of a tower type
+    public int GetTowerPrice(TowerType towerType) //--------------------------------Waiting on Asher's part to finish
+    {
+        return (from towerCost in TowerCosts
+                where towerCost.TowerType == towerType
+                select towerCost.Cost).FirstOrDefault();
+    }
+}
