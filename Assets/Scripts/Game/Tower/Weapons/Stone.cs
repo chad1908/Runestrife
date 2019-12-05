ccf051c1fede9cfa0f3e2929d0bcf31aa81cd259
Humza Khan <humza.khan@msn.com>
2019-12-05 11:25:23 -0500

    Added Stone Script

---
+++ Assets/Scripts/Game/Tower/Weapons/Stone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : FollowingProjectile
+{
+    //amount of damage that gets inflited to the enemy on impact
+    public float damage;
+
+    //Override FollowingProjectile's OnHitEnemy() method
+    protected override void OnHitEnemy()
+    {
+        //Deal damage to the enemy and destroy the projectile
+        enemyToFollow.TakeDamage(damage);
+        Destroy(gameObject);
+    }
+}
